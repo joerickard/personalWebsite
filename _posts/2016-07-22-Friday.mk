c21754cd7281dfb46eb5f7b230bd4f2647df6bed
Joe Rickard <joe.s.rickard@gmail.com>
2016-07-22 12:48:20 -0500

    Create 2016-07-22-Friday.mk

---
+++ _posts/2016-07-22-Friday.mk
@@ -0,0 +1,8 @@
+---
+layout: post
+title: "Friday"
+date: 2016/07/22
+---
+Travel Day
+---
+Well Everything was canceled when I arrived at DIA Thursday evening. I managed to get a refund on the Southwest flight, and booked a United trip to get me to Chicago by morning via S Dakota. This flight landed me in a small remote airport at midnight, expecting to leave for Chicago very early the next morning, however that second leg was promptly cancelled. And the airport closed, leaving me literally sitting out front of this empty lit building for four hours waiting for United employees to show back up. The morning shift was able to rebook me to Chicago via Minnesota, although I was no longer going to make it to the Mac race start, the driving force for the trip. I am sitting in Minnesota as I type, excited that on this last flight, (which is delayed significantly), I am at least both an aisle seat and in an exit row. This is the kind of day that makes me glad I travel so infrequently.
